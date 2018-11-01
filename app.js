955f307ed82f3513bc2ee2edba1abdf66f6db967
dooleyb1 <dooleyb1@tcd.ie>
2018-11-01 15:42:24 +0000

    changed proxy method

--- app.js
+++ app.js
@@ -4,21 +4,12 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 
 const { routes } = require('./proxyconfig.json');
-const port = 8000;
+const port = 3000;
 
 // Host static personal site
-app.use('/', express.static('public'));
+app.use('/', express.static('public/personal-site'));
 
-// Host routes for personal apps as defined in
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
-            }
-        })
-    );
-}
+app.use('/github-access', proxy({target: 'http://localhost:3006', changeOrigin: true}));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
