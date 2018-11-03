ea37b41ec5744d2b7959604ed40aae50dc03560a
dooleyb1 <dooleyb1@tcd.ie>
2018-11-03 15:34:17 +0000

    removed proxy, testing jenkins

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const proxy = require('http-proxy-middleware');
 
 const app = express();
 
-const { routes } = require('./proxyconfig.json');
-const port = 3000;
+const port = 8080;
 
 // Host static personal site
 app.use('/', express.static('public/personal-site'));
-
-app.use('/github-access', proxy({target: 'http://localhost:3006', changeOrigin: true}));
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
